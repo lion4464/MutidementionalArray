d2cd9dab98399c5e04669aa94eaa540a9716910b
talablariga <talablarigabinoan@gmail.com>
2021-07-13 14:51:02 +0500

    (Sort students on grades) #2

--- eighth_03.java
+++ eighth_03.java
@@ -1,25 +1,23 @@
 import java.util.*;
 class eight_03{
 	public static void main(String[] args) {
-				Scanner input=new Scanner(System.in);
+		Scanner input=new Scanner(System.in);
 	// ------------------------------------------------------
 	//Enter amount of questions
 	// ------------------------------------------------------
-					System.out.print("Enter amount of Questions:");
-				int amountQuestions=input.nextInt();  
+		System.out.print("Enter amount of Questions:");
+		int amountQuestions=input.nextInt();  
 	// ------------------------------------------------------
 	//Enter amount of Students  
 	// ------------------------------------------------------
-				System.out.print("Enter amount of Students:");
-				int amountStudents=input.nextInt();
+		System.out.print("Enter amount of Students:");
+		int amountStudents=input.nextInt();
 		char[][] Newarr=new char[amountStudents][amountQuestions];
-
 	// ------------------------------------------------------
 	// Initializing element of multi Arr
 	// ------------------------------------------------------
-
 		for (int i=0;i<Newarr.length;i++) {
-				System.out.print("Enter"+(i+1)+"-Student answer:");
+			System.out.print("Enter"+(i+1)+"-Student answer:");
 			for (int j=0;j<amountQuestions;j++) {
 				Newarr[i][j]=input.next().charAt(0);
 				}
@@ -32,12 +30,6 @@ class eight_03{
 		for (int k=0;k<AnswerArr.length;k++) {
 			AnswerArr[k]=input.next().charAt(0);
 		}
-
-		// for (int i=Newarr.length-1;i>=0;i--) {
-		// 	for (int e:Newarr[i]) {
-		// 		System.out.print(e+" ");
-		// 	}
-		// }
 	// ------------------------------------------------------
 	// Call methed to find amount of right answer!!!!
 	// ------------------------------------------------------
