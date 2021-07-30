016d0543c3e0dcf0c9ea7fe385235984e19f9f5c
talablariga <talablarigabinoan@gmail.com>
2021-07-30 12:00:44 +0500

    #2

---
+++ eighth_05.java
@@ -0,0 +1,34 @@
+public class eighth_05{
+	public static void main(String[] args) {
+		//----------------ennounse a matrix-----------
+		double[][] a_matrix={
+			{1,4,3},
+			{2,3,5}
+		};
+		//-----------------ennounse b matrix-------
+		double[][] b_matrix={
+			{4,1,2},
+			{3,2,0}
+		};
+		// ----------Call addMatrix method----------
+		double[][] c=addMatrix(a_matrix,b_matrix);
+		//-----------Print all "c" array's elements------
+		for (int i=0;i<c.length;i++) {
+			for (int j=0;j<c[i].length;j++) {
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+	}
+	// addMatrix method
+	public static double[][] addMatrix(double[][] a, double[][] b){
+		// create new MULTIDEMENTION ARRAY
+		double[][] c=new double[a.length][a[0].length];
+		for (int i=0;i<a.length;i++) {
+			for (int j=0;j<a[i].length;j++) {
+				c[i][j]=a[i][j]+b[i][j];
+			}
+		}
+		return c;
+	}
+}
